fc8e4c317bad6111d7a1b46728aefc0c5842b10e
Kevin Bruce Mulinda <89967620+Dukent29@users.noreply.github.com>
2025-05-25 19:03:24 +0200

    updated testimonial

--- template-parts/testimonials.php
+++ template-parts/testimonials.php
@@ -4,5 +4,9 @@
             <h2 class="title">Ce Que Disent Nos Clients</h2>
             <p class="com-subtitle">Découvrez pourquoi nos clients nous font confiance pour leurs besoins en matière de sécurité.</p>
         </div>
+        <div class="testimonials-wrapper">
+            <?php echo do_shortcode('[testimonials]');
+            ?>
+        </div>
     </div>
 </section>
